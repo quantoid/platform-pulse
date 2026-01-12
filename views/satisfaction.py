5a1cbae40a7da7d4a3538280e7eb967f10314fcc
mike <quantoid@mac.com>
2026-01-12 12:02:15 +1000

    Tweak wording.

--- views/satisfaction.py
+++ views/satisfaction.py
@@ -9,7 +9,7 @@ st.warning(
     "NPS is not great for internal user satisfaction because:"
     "\n- they didn't choose it;"
     "\n- they can't switch to something else; and"
-    "\n- their time and effort is more important to us."
+    "\n- their time and effort is more important to us than recommending to others."
     "\n\nCSAT is better, where users are asked how satisfied they are with the tool when they complete a task."
     " This data has to be supported by usage/adoption metrics."
     " Need to be careful about survey fatigue and gaming the system.",
